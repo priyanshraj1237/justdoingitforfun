ff5650b6c0018ad07084e33851e42c96cf8bfbfc
Priyanshu Ranjan <154821417+priyanshraj1237@users.noreply.github.com>
2026-09-13 20:20:15 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/reverse-string/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        def reverse(i,j):
+            if i>j :
+                return s
+            else :
+                s[i],s[j]=s[j],s[i]
+                i +=1
+                j-=1
+                reverse(i,j)
+        i=0
+        j=len(s)-1
+        reverse(i,j)
+        
+        

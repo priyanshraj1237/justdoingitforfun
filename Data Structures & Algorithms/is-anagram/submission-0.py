1fe6aab2253cff503431c2e49df346a178439152
Priyanshu Ranjan <154821417+priyanshraj1237@users.noreply.github.com>
2026-09-14 02:15:20 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        h1={}
+        h2={}
+        for i in s:
+            if i in h1:
+                h1[i] +=1
+            else:
+                h1[i]=1 
+        for i in t:
+            if i in h2:
+                h2[i] +=1
+            else:
+                h2[i]=1 
+        if h1==h2:
+            return True
+        else:
+            return False
+        
+        
+        
